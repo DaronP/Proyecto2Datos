7d878f9537992ecb08c8d223b0a816d2a2693d0c
Oliversinn <Oliversinn@192.168.0.12>
2018-05-02 21:25:04 -0600

    Interfaz de Map

---
+++ DatosProyecto2/src/proyecto2/MapInterface.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+
+
+
+public interface MapInterface<K,V>
+{
+   public boolean equals (Object o);
+   // post: returns true iff this entry <K,V> is equal to object o
+
+   public K getKey();
+   // post: returns the key K of this entry
+
+   public V getValue();
+   // post: returns the value V of this entry
+
+   public int hashCode();
+   // post: returns the hashcode for the key
+   
+   public V setValue(V value);
+   // post: replaces the Value of this entry
+   
+ }
